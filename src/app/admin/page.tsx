3d9fb8debde3a536123ecce8984a25bd2f6c9573
mariusherlea <mariusherlea@gmail.com>
2024-04-15 12:27:38 +0300

    secure admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -7,18 +7,6 @@ import {
 } from '@/components/ui/card';
 import db from '@/db/db';
 import { formatNumber, formatCurrency } from '@/lib/formatters';
-import { unstable_noStore as noStore } from 'next/cache';
-
-export function getEnvVariable(name: string) {
-  // Using noStore prevents this function from being rendered statically
-  // This is a problem because the environment variables need to change between development and production
-  noStore();
-  const variable = process.env[name];
-  if (!variable) {
-    throw new Error('Missing environment variable for ' + name);
-  }
-  return variable;
-}
 
 async function getSalesData() {
   const data = await db.order.aggregate({
