36365ad5ad95daf50b241203ddd1541cda4dde65
mariusherlea <mariusherlea@gmail.com>
2024-04-16 09:30:58 +0300

    customer products page

---
+++ src/app/(customerFacing)/products/page.tsx
@@ -0,0 +1,43 @@
+import { ProductCardSkeleton, ProductCard } from '@/components/ProductCard';
+import { Suspense } from 'react';
+import db from '@/db/db';
+
+function getProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: {
+      name: 'asc',
+    },
+  });
+}
+
+export default function ProductsPage() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <Suspense
+        fallback={
+          <>
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+          </>
+        }>
+        <ProductsSupense />
+      </Suspense>
+    </div>
+  );
+}
+
+async function ProductsSupense() {
+  const products = await getProducts();
+  return (
+    <>
+      {products.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
+    </>
+  );
+}
