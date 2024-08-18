cac69c51b4ad05c076c9834cbc4a7e01b9ca7372
Herlea Marius <mariusherlea@gmail.com>
2024-08-18 12:55:57 +0300

    hero and font

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,10 @@
-import { Inter } from "next/font/google";
+import { Raleway } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
 
 import Footer from "./_components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const raleway = Raleway({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={raleway.className}>
         <Header />
 
         {children}
