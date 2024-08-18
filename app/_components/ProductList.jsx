703841102b38fbbfc5609ca83eef03015af07730
Herlea Marius <mariusherlea@gmail.com>
2024-08-18 15:44:26 +0300

    add pricing to product list

--- app/_components/ProductList.jsx
+++ app/_components/ProductList.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 function ProductList({ productList }) {
+  console.log(productList);
   return (
     <div className="text-white">
       {productList.map((product) => (
         <div key={product.id}>
           <h2>{product.attributes?.title}</h2>
+          <p>{product.attributes?.pricing}</p>
         </div>
       ))}
     </div>
