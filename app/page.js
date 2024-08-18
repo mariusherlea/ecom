ff08ddf1c6ab85e1c495bc220e2916db4be4a61c
Herlea Marius <mariusherlea@gmail.com>
2024-08-18 13:00:26 +0300

    ProductList ProductSection` `

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
 import Hero from "./_components/Hero";
+import ProductSection from "./_components/ProductSection";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      {/* Latest products */}
+      <ProductSection />
+      {/* Project source code */}
+      {/* Icons packs */}
     </div>
   );
 }
