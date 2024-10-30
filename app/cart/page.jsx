a6459cd5e655d850c5ba6f2f121ef0850c410082
Herlea Marius <mariusherlea@gmail.com>
2024-10-30 10:22:19 +0200

    total price of cart

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,13 +1,24 @@
 "use client";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { CartContext } from "../_context/CartContext";
 import Image from "next/image";
+import { get } from "http";
 
 function Scart() {
   const { cart, setCart } = useContext(CartContext);
 
   const getImageUrl = (path) => {
     return path?.startsWith("http") ? path : `http://localhost:1337${path}`;
+  };
+
+  const getTotalAmount = () => {
+    let totalAmount = 0;
+    cart.forEach((element) => {
+      totalAmount =
+        totalAmount +
+        Number(element.attributes.products.data[0].attributes.pricing);
+    });
+    return totalAmount;
   };
 
   return (
@@ -59,15 +70,12 @@ function Scart() {
                           </dt>
                         </div>
 
-                        <div>
-                          <dt className="inline">
-                            ${product?.pricing || "0.00"}
-                          </dt>
-                        </div>
+                        <div></div>
                       </dl>
                     </div>
 
                     <div className="flex flex-1 items-center justify-end gap-2">
+                      <dt className="inline">${product?.pricing || "0.00"}</dt>
                       <button className="text-gray-600 transition hover:text-red-600">
                         <span className="sr-only">Remove item</span>
 
@@ -95,49 +103,11 @@ function Scart() {
             <div className="mt-8 flex justify-end border-t border-gray-100 pt-8">
               <div className="w-screen max-w-lg space-y-4">
                 <dl className="space-y-0.5 text-sm text-gray-700">
-                  <div className="flex justify-between">
-                    <dt>Subtotal</dt>
-                    <dd>£250</dd>
-                  </div>
-
-                  <div className="flex justify-between">
-                    <dt>VAT</dt>
-                    <dd>£25</dd>
-                  </div>
-
-                  <div className="flex justify-between">
-                    <dt>Discount</dt>
-                    <dd>-£20</dd>
-                  </div>
-
                   <div className="flex justify-between !text-base font-medium">
                     <dt>Total</dt>
-                    <dd>£200</dd>
+                    <dd>${getTotalAmount()}</dd>
                   </div>
                 </dl>
-
-                <div className="flex justify-end">
-                  <span className="inline-flex items-center justify-center rounded-full bg-indigo-100 px-2.5 py-0.5 text-indigo-700">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="-ms-1 me-1.5 size-4"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z"
-                      />
-                    </svg>
-
-                    <p className="whitespace-nowrap text-xs">
-                      2 Discounts Applied
-                    </p>
-                  </span>
-                </div>
 
                 <div className="flex justify-end">
                   <a
